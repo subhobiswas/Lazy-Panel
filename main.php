5527f79c4f806850cec08b2d492a8a3b3c485cd7
subhobiswas <subhobiswas705971@gmail.com>
2021-07-09 17:17:28 +0530

    commit

---
+++ main.php
@@ -0,0 +1,9 @@
+<?php include 'header.php'; ?>
+<div class="container-fluid">
+	<!-- DataTales Example -->
+	<div class="card shadow mb-4">
+		<div class="card-header py-3">
+		</div>
+	</div>
+</div>
+<?php include 'footer.php'; ?>
